e7addbbc6312f014f233537a212e531e617a39e0
MateusPires11 <mateusoliveirapires509@gmail.com>
2026-08-10 23:21:30 -0300

    init commit

---
+++ 1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    system("chcp 65001 > nul"); 
+
+    int n1,i,n2;
+
+    printf("Digite o tamanho do vetor: ");
+    scanf("%i",&n1);
+
+    int vet[n1];
+
+    printf("Digite os valores para o vetor: \n");
+    
+    for ( i = 0; i < n1; i++)
+    {
+        scanf("%i",&n2);
+        vet[i] = n2;
+    }
+    printf("os valores são \n");
+    for ( i = 0; i < n1; i++)
+    {
+       printf("%i ", vet[i]);
+    }
+    
+    
+}
