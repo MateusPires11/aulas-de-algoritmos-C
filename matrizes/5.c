79c9340a37361f0f10c6258c344859bffe7b0256
MateusPires11 <mateusoliveirapires509@gmail.com>
2026-08-25 23:48:13 -0300

    update

--- matrizes/5.c
+++ matrizes/5.c
@@ -6,34 +6,35 @@
 
 int main()
 {
-
-    int matriz[4][4];
-    int result[4];
-    int vetor[4];
-
-    int i, j, v, v1, soma;
+    int i, j, v, v1, soma, t;
+    
+    printf("Digite um tamanho para a matriz ");
+    scanf("%i", &t);
+    int matriz[t][t];
+    int result[t];
+    int vetor[t];
 
     srand(time(NULL));
 
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < t; i++)
     {
-        for (j = 0; j < 4; j++)
+        for (j = 0; j < t; j++)
         {
             matriz[i][j] = rand() % 10;
         }
     }
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < t; i++)
     {
         vetor[i] = rand() % 10;
     }
 
     v = 0;
     v1 = 0;
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < t; i++)
     {
         soma = 0;
 
-        for (j = 0; j < 4; j++)
+        for (j = 0; j < t; j++)
         {
 
             soma = soma + matriz[i][j];
@@ -44,27 +45,28 @@ int main()
     }
 
     printf("\nMatriz:\n");
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < t; i++)
     {
-        for (j = 0; j < 4; j++){
+        for (j = 0; j < t; j++)
+        {
             printf("%2d ", matriz[i][j]);
         }
         printf("\n");
     }
 
     printf("\nVetor: ");
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < t; i++)
     {
         printf("%2d ", vetor[i]);
     }
 
     printf("\n");
     printf("\nSoma: ");
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < t; i++)
     {
 
         Sleep(500);
-        printf( "%2d ", result[i]);
+        printf("%2d ", result[i]);
     }
 
     getche();
