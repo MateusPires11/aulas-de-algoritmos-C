8b41ef09f502c43f783637deb76c5f057ae46e79
MateusPires11 <mateusoliveirapires509@gmail.com>
2026-08-27 18:25:42 -0300

    segundoexe

--- matrizes/6.c
+++ matrizes/6.c
@@ -9,7 +9,7 @@ int main()
 
     int matriz[3][3];
 
-    int i,j,num;
+    int i, j, num, maior;
 
     srand(time(NULL));
 
@@ -18,34 +18,40 @@ int main()
         for (j = 0; j < 3; j++)
         {
             matriz[i][j] = rand() % 10;
-            
         }
     }
 
     printf("Digite um numero ");
-    scanf("%i",&num);
-
-     for (i = 0; i < 3; i++)
+    scanf("%i", &num);
+    maior = 0;
+    for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 3; j++)
         {
             if (num == matriz[i][j])
             {
-                printf("posicao %i %i",i,j);
+                printf("posicao %i %i", i, j);
+                printf("\n");
             }
-    
+            else if (matriz[i][j] > num)
+            {
+
+                maior++;
+            }
         }
     }
-
 
     for (i = 0; i < 3; i++)
     {
         printf("\n");
 
         for (j = 0; j < 3; j++)
-
+        {
             printf("%i", matriz[i][j]);
+        }
     }
+    printf("\n");
+    printf("%i sao maiores", maior);
 
     getche();
 }
