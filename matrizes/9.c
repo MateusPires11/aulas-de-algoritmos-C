ceca5138127d7fcdc33e2eabf46425406bd4a80d
MateusPires11 <mateusoliveirapires509@gmail.com>
2026-08-28 14:02:38 -0300

    ultimo exe

--- matrizes/9.c
+++ matrizes/9.c
@@ -1,13 +1,44 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <conio.h>
 #include <windows.h>
 #include <time.h>
 
+int main()
+{
 
-int main(){
+    int vet[10];
+    int cont, i, num;
 
-    int vet [10];
+    srand(time(NULL));
 
+    for (i = 0; i < 10; i++)
+    {
+        vet[i] = rand() % 10;
+    }
 
-    
+    printf("digite um numero de 1 a 10: ");
+    scanf("%i", &num);
+    cont = 0;
+    for (i = 0; i < 10; i++)
+    {
+        if (vet[i] == num)
+        {
+            printf("numero encontrado na posicao %i \n", i);
+            cont++;
+        }
+    }
+    printf("Vetor: ");
+    for (i = 0; i < 10; i++)
+    {
+
+        printf("%i ", vet[i]);
+    }
+    printf("\n");
+    if (cont == 0)
+    {
+        printf("numero nao foi encontrado no vetor");
+    }
+
+    getch();
 }
